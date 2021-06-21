07467c8d07bba2d04fce611990a568225b038287
delbfinych <dmi.villione@gmail.com>
2021-06-21 12:53:16 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 ReactDOM.render(
     <Provider store={store}>
-        <MemoryRouter>
+        <BrowserRouter>
             <App />
-        </MemoryRouter>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
